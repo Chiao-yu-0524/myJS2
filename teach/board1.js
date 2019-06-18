d43041f7d19ee1371ad7d1cea592e90ef4b1d0ca
詹喬羽 <sam@zhanqiaoyudeMBP.r2101.tciii.org.tw>
2019-06-18 17:20:27 +0800

    yes

---
+++ teach/board1.js
@@ -0,0 +1,19 @@
+let var1; //宣告
+document.write(var1 + "<br>");
+var1 = 123;
+document.write(typeof(var1) + "<br>"); //看值的型態
+var1 = 12.3;
+document.write(typeof(var1) + "<br>");
+var1 = true;
+document.write(typeof(var1) + "<br>");
+var1 = "board";
+document.write(typeof(var1) + "<br>");
+
+const name = "BOARD";
+
+
+document.write("my name is " + name + "<br>");
+document.write(`My name is ${name} <br>`);
+
+
+
