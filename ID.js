c59fbb24fb5fbf73c82948543575ec4224680b16
詹喬羽 <sam@zhanqiaoyudeMBP.r2101.tciii.org.tw>
2019-06-19 16:47:42 +0800

    功課完成

--- ID.js
+++ ID.js
@@ -1,15 +1,13 @@
-    //檢查身分證字號
-function checkTWId(id) {
-
-    let ret = false ;
-    let regex = /^[A-Z][12][0-9]{8}$/ ;  //身分證得限制條件
-    let result = id.match(regex); //判斷有英文字母
-    if (result ==null ) {
-        let letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO'; 
-        let c12 = id.substr(0,1); //取出第一碼的英文
-        let n12 = letters.indexOf(c12) + 10 ; //判斷第一馬的值
-        let n1 = parseInt(n12 / 10) ; //第一碼取出十位數
-        let n2 = n12 % 10 ; // 第一碼取出個位數
+function checkTWId(id){
+    let ret = false;
+    let regex = /^[A-Z][12][0-9]{8}$/;
+    let result = id.match(regex);
+    if (result != null){
+        let letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
+        let c12 = id.substr(0,1);
+        let n12 = letters.indexOf(c12) + 10;
+        let n1 = parseInt(n12 / 10);
+        let n2 = n12 % 10;
         let n3 = parseInt(id.substr(1,1));
         let n4 = parseInt(id.substr(2,1));
         let n5 = parseInt(id.substr(3,1));
@@ -19,11 +17,12 @@ function checkTWId(id) {
         let n9 = parseInt(id.substr(7,1));
         let n10 = parseInt(id.substr(8,1));
         let n11 = parseInt(id.substr(9,1));
-        let sum = n1*1 + n2*9 + n3*8 + n4*7 + n5*6 + n6*5 + n7*4 + n8*3 + n9*2 + n10*1 + n11*1  ;
+        let sum = n1*1 + n2*9 + n3*8 + n4*7 + n5*6 + n6*5 +
+                n7*4 + n8*3 + n9*2 + n10*1 + n11*1;
+        ret = sum % 10 == 0;
     }
-    return true ;
+    return ret;
 }
-
 
 function createAnswer(n = 3){
     let poker = [];
@@ -40,7 +39,7 @@ function createAnswer(n = 3){
 function checkAB(ans, gus){
     let a = 0, b = 0;
     for (let i = 0; i< gus.length; i++){
-        if (gus.charAt(i) == ans.charAt(i)){ //
+        if (gus.charAt(i) == ans.charAt(i)){
             a++;
         }else if (ans.indexOf(gus.charAt(i)) >= 0){
             b++;
@@ -49,3 +48,4 @@ function checkAB(ans, gus){
 
     return a +'A' + b + 'B';
 }
+
